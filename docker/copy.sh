02d6b9993d1fa461ef3afac7919775ee20b134aa
Mitsuhazxy <40455446+Mitsuhazxy@users.noreply.github.com>
2026-08-14 20:53:54 +0800

    docs: update project memory with server dirs & git history; fix copy.sh SQL filenames

--- docker/copy.sh
+++ docker/copy.sh
@@ -9,8 +9,8 @@ usage() {
 
 # copy sql
 echo "begin copy sql "
-cp ../sql/ry_20260402.sql ./mysql/db
-cp ../sql/ry_config_20260311.sql ./mysql/db
+cp ../sql/ry_20260417.sql ./mysql/db
+cp ../sql/ry_config_20260611.sql ./mysql/db
 
 # copy html
 echo "begin copy html "
